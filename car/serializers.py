5d205554af4ab33fd08b811ac8f142c47016a13e
Ihor Zasimenko <boxletter2011@gmail.com>
2026-03-11 17:36:18 +0200

    Fix CarSerializer: add id field and adjust horse_powers validation

--- car/serializers.py
+++ car/serializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 
 
 class CarSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)  # додали id
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField(min_value=1, max_value=2000)
+    horse_powers = serializers.IntegerField(min_value=1, max_value=1999)  # максимум 1999
     is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(allow_null=True, required=False)
