edf05b1e6d1e8d21d5ae921447385ebffbe673f9
Ihor Zasimenko <boxletter2011@gmail.com>
2026-03-11 17:53:34 +0200

    Finalize serializer and main functions to pass all tests

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import json
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return json.dumps(serializer.data, separators=(",", ":")).encode("utf-8")
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json_bytes: bytes) -> Car:
+    data = json.loads(json_bytes.decode("utf-8"))
+
+    serializer = CarSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+
+    validated = serializer.validated_data
+
+    car = Car(**validated)
+
+    if "id" in data:
+        car.id = data["id"]
+
+    return car
